34c095e219cf667a1e838fedfa9bea69c90c4739
kazuma1989 <kazuma.ebina@quipper.com>
2021-06-02 17:04:34 +0900

    feat: Init React App

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Hello</h1>
+  </React.StrictMode>,
+
+  document.getElementById("root")
+);
